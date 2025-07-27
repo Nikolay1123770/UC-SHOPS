a51f586eb62b4424a9101daa26e7dd450e434413
Nikolay1123770 <131786139+Nikolay1123770@users.noreply.github.com>
2025-07-28 00:42:51 +0300

    run.py

--- run.py
+++ run.py
@@ -1,16 +1,26 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
-import os
+import asyncio
+from bot import start_bot  # импортируем запуск бота из bot.py
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup_event():
+    # Запускаем бота в фоне при старте FastAPI
+    asyncio.create_task(start_bot())
+
 
 @app.get("/")
 def read_root():
     return {"status": "ok", "message": "UC SHOP API запущен."}
 
+
 @app.get("/privacy-policy")
 def privacy_policy():
     return FileResponse("html/privacy-policy.html", media_type="text/html")
+
 
 @app.get("/terms-of-service")
 def terms_of_service():
