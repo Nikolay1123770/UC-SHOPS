fc4e03c92712d4b2cb169d8bbd15a6d2867b8fc6
Nikolay1123770 <131786139+Nikolay1123770@users.noreply.github.com>
2025-07-28 00:51:05 +0300

    run.py

--- run.py
+++ run.py
@@ -1,19 +1,1 @@
-from fastapi import FastAPI
-import uvicorn
-import asyncio
 
-from start_bot import main as start_bot_main  # Импорт функции запуска бота
-
-app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "UC SHOP is running"}
-
-
-# Запуск бота параллельно с сервером FastAPI
-@app.on_event("startup")
-async def startup_event():
-    loop = asyncio.get_event_loop()
-    loop.create_task(start_bot_main())
